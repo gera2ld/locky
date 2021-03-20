a16bc4c5c1e133e51a9905276763a816e8c07beb
Gerald <gera2ld@live.com>
2021-03-20 13:06:35 +0800

    feat: support tencent mirror

--- test/yarn.js
+++ test/yarn.js
@@ -29,13 +29,22 @@ core-js@^3.1.3:
   version "3.1.3"
   resolved "https://registry.npm.taobao.org/core-js/download/core-js-3.1.3.tgz#95700bca5f248f5f78c0ec63e784eca663ec4138"
 `,
+  tencent: `
+"@babel/runtime@^7.0.0", "@babel/runtime@^7.2.0", "@babel/runtime@^7.3.4", "@babel/runtime@^7.4.5":
+  version "7.4.5"
+  resolved "https://mirrors.cloud.tencent.com/npm/@babel%2fruntime/-/runtime-7.4.5.tgz#582bb531f5f9dc67d2fcb682979894f75e253f12"
+
+core-js@^3.1.3:
+  version "3.1.3"
+  resolved "https://mirrors.cloud.tencent.com/npm/core-js/-/core-js-3.1.3.tgz#95700bca5f248f5f78c0ec63e784eca663ec4138"
+`,
 };
 
 function test(source, target) {
   console.info(`Transform from ${source} to ${target}`);
   const actual = transform(locks[source], target);
   const expected = locks[target];
-  assert.equal(actual, expected);
+  assert.strictEqual(actual, expected);
 }
 
 const keys = Object.keys(locks);
